8dfebe059a9870b1515a2fe3cd773be727e64b88
dragenkit <marvinauner47@gmail.com>
2025-01-07 20:09:06 +0100

    add View

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
+
 public class ToDoList {
     public static void main(String[] args) {
-
+        ToDoList toDoList = new ToDoList();
+    }
+    public ToDoList() {
+        JFrame todoView = new JFrame();
+        todoView.setTitle("To Do List");
+        todoView.setResizable(false);
+        todoView.setSize(600,700);
+        todoView.setVisible(true);
     }
 }
