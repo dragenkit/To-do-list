b137efd4441deafa933d87cd87bdcc0d077ac3dd
dragenkit <marvinauner47@gmail.com>
2025-01-07 22:07:41 +0100

    add textfield

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class ToDoList {
@@ -9,7 +10,16 @@ public class ToDoList {
         JFrame todoView = new JFrame();
         todoView.setTitle("To Do List");
         todoView.setResizable(false);
-        todoView.setSize(600,700);
+        todoView.setPreferredSize(new Dimension(400,600));
+        todoView.setLayout(new GridLayout(1, 1));
+
+
+        JTextField textFieldTodo = new JTextField();
+        textFieldTodo.setPreferredSize(new Dimension(400,40));
+        todoView.add(textFieldTodo);
+
+        todoView.pack();
         todoView.setVisible(true);
+
     }
 }
