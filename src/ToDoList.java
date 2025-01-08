6f48f1e65abac4b99ef11f5fa82305904398a7c0
dragenkit <marvinauner47@gmail.com>
2025-01-08 02:50:34 +0100

    the button and textfield are now next to eachother

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -9,29 +9,34 @@ public class ToDoList implements ActionListener {
         ToDoList toDoList = new ToDoList();
     }
 
+    JFrame todoView;
     JButton button;
     JTextField textFieldTodo;
-    JFrame todoView;
+    JPanel inputPanel;
+
 
     public ToDoList() {
         todoView = new JFrame();
         todoView.setTitle("To Do List");
         todoView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        todoView.setResizable(false);
+        //todoView.setResizable(false);
         todoView.setSize(400,700);
         todoView.setLayout(new BoxLayout(todoView.getContentPane(), BoxLayout.Y_AXIS));
+
+        inputPanel = new JPanel();
 
         ImageIcon image = new ImageIcon("Assets/appIcon.png");
         todoView.setIconImage(image.getImage());
 
         button = new JButton("Add");
         button.addActionListener(this);
+        inputPanel.add(button);
 
-        todoView.add(button);
         textFieldTodo = new JTextField();
         textFieldTodo.setPreferredSize(new Dimension(400,40));
-        todoView.add(textFieldTodo);
+        inputPanel.add(textFieldTodo);
 
+        todoView.add(inputPanel);
         todoView.pack();
         todoView.setVisible(true);
 
@@ -47,6 +52,7 @@ public class ToDoList implements ActionListener {
 
     public void getTodoElement(String text) {
         JLabel todoElement = new JLabel(text);
+
         todoView.add(todoElement);
         todoView.revalidate();
         todoView.repaint();
